94983f6816741e1d2db6e55efb3951e6c9900f36
ghchae <cvxgihyun@gmail.com>
2020-12-26 01:19:23 +0900

    memoryMemberRepository 테스트코드 추가

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -10,7 +10,7 @@ public class MemoryMemberRepository implements MemberRepository {
     private static long sequence = 0L;
 
     @Override
-    public Member Save(Member member) {
+    public Member save(Member member) {
         member.setId(++sequence);
         store.put(member.getId(), member);
         return member;
@@ -29,5 +29,9 @@ public class MemoryMemberRepository implements MemberRepository {
     @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
     }
 }
---
+++ src/test/java/hello/hellospring/HelloSpringApplicationTest.java
@@ -0,0 +1,11 @@
+package hello.hellospring;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class HelloSpringApplicationTest {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringApplication.class, args);
+    }
+}
