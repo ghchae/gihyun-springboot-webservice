8b7be408ced0e1f9c6e1b0a2944e13292bbfa6ea
ghchae <cvxgihyun@gmail.com>
2021-01-12 23:56:33 +0900

    timeTraceaop적용

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -40,8 +40,4 @@ public class SpringConfig {
         //return new JpaMemberRepository(em);
 
     //}
-    @Bean
-    public TimeTraceAop TimeTraceAop() {
-        return new TimeTraceAop();
-    }
 }
